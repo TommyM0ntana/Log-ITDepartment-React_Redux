fe667c92787de2d2113e7c85d3fc8c08c091283b
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-04-16 01:32:55 +0200

    add preloader materialize

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,40 +1,33 @@
 import React, { useState, useEffect } from "react";
 import LogItem from "./LogItem";
-import M from "materialize-css/dist/js/materialize.min.js";
+import Preloader from '../layouts/Preloader'
 
 const Logs = () => {
-  //set the state for logs and loading
   const [logs, setLogs] = useState([]);
   const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    getLogs();
-    //eslint-disable-next-line
-  }, []);
-  //add the setLogs function fetching from the back end
-  const getLogs = async () => {
-    setLoading(true);
-    const res = await fetch("/logs");
-    const data = await res.json();
 
-    //setting the new state when it loads to get and give  the data
-    setLogs(data);
-    setLoading(false);
-  };
+  useEffect(() => {
+    setLoading(true);
+    fetch("/logs")
+      .then(res => res.json())
+      .then(setLogs)
+      .then(() => setLoading(false));
+  }, [setLoading, setLogs]);
 
   if (loading) {
-    return <h4> Loading...</h4>;
+    return <Preloader />;
   }
 
   return (
-    <ul className='collection with-header'>
+    <ul className='collection with-header' style={{ marginTop: "50px" }}>
       <li className='collection-header'>
-        <h4 className='center'>Logs</h4>
+        <h4 className='center'>System Logs</h4>
       </li>
       {!loading && logs.length === 0 ? (
-        <p className='center'>No logs to show..</p>
+        <p className='center'>No logs to show...</p>
       ) : (
-        logs.map(log => <LogItem log={log} key={log.id} />)
-      )}
+          logs.map(log => <LogItem log={log} key={log.id} />)
+        )}
     </ul>
   );
 };
